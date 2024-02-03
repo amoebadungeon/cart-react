10d7283dbd1d672786a5e8f63ff564e72901aedc
mustaqeem <mustaqeemsopee@gmail.com>
2024-02-04 07:50:02 +0800

    commit lagi skli href

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 import { ListingPage , ProductPage,CartPage } from './pages'
 import { useSelector } from 'react-redux'
 
@@ -13,8 +13,8 @@ function App() {
         <div className='website-header'><img src={`${process.env.PUBLIC_URL}/picture/logo.png`} alt="list"/></div>
          <div className='menu-header'>
           <ul className='list-menu'>
-            <li><a href={`${process.env.PUBLIC_URL}/`}><img src={`${process.env.PUBLIC_URL}/picture/iconList.png`} alt="list"/></a></li>
-            <li><a href={`${process.env.PUBLIC_URL}/cart`}><img src={`${process.env.PUBLIC_URL}/picture/cart.png`}  alt="list"/></a>
+            <li><a href={'${process.env.PUBLIC_URL}/'}><img src={`${process.env.PUBLIC_URL}/picture/iconList.png`} alt="list"/></a></li>
+            <li><a href={'${process.env.PUBLIC_URL}/cart'}><img src={`${process.env.PUBLIC_URL}/picture/cart.png`}  alt="list"/></a>
             {itemCount > 0 && <span className="cart-count">{itemCount}</span>}</li>
             <li><img src={`${process.env.PUBLIC_URL}/picture/setting.png`}  alt="list"/></li>
           </ul>
